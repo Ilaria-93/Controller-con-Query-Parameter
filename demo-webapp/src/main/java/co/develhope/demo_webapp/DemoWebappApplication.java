91f891ce2dc60b36ef4ef6b87750f51d464b6114
Ilaria Faleschini <150346358+Ilaria-93@users.noreply.github.com>
2024-06-27 10:33:22 +0200

    Add files via upload

---
+++ demo-webapp/src/main/java/co/develhope/demo_webapp/DemoWebappApplication.java
@@ -0,0 +1,12 @@
+package co.develhope.demo_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebappApplication.class, args);
+	}
+}
